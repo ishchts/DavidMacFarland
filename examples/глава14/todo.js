68970bf835661d7f46ec976e206965ea6dda50a8
Ilya <i1ya1l@yandex.ru>
2018-07-30 20:19:24 +0300

    to-do list

--- examples/глава14/todo.js
+++ examples/глава14/todo.js
@@ -1,3 +1,35 @@
 $(document).ready(function(e) {
+	$('.js-popUp').dialog({
+		modal : true,
+		autoOpen : false,
+		buttons : {
+			"Добавить" : function () {
+				var $input = $('form').find('#task');
+				if ($input.val()) {
+					var newHtml = ('<li>');
+					newHtml += '<span class="one">' + '</span>';
+					newHtml += '<span class="two">' + '</span>';
+					newHtml += '<span class="three">' + '</span>' + '</li>';
+					var $newHtml = $(newHtml);
+					$newHtml.find('.three').text($input.val());
+					$newHtml.hide();
+					$('#todo-list').append($newHtml);
+					$newHtml.fadeIn();
+				} 
+			},
+			"Не добавить" : function () {
+				$(this).dialog('close');
+			}
+		}
+	});
+
+	$('.js-addBtn').button({
+		icons : {
+			primary : "ui-cion-circle-plus"
+		}
+	}).click(function(e){
+		e.preventDefault();
+		$('.js-popUp').dialog('open');
+	});
 
 }); // end ready
